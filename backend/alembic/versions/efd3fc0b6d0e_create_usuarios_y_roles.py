9a8677855ee23e956cac97585d531c36511ba167
Victor Contreras <vcontreras@gmail.com>
2025-06-19 20:22:58 -0300

    refactor: simplify linter command in CI workflow and remove unused imports in migration script

--- backend/alembic/versions/efd3fc0b6d0e_create_usuarios_y_roles.py
+++ backend/alembic/versions/efd3fc0b6d0e_create_usuarios_y_roles.py
@@ -7,10 +7,6 @@ Create Date: 2025-06-16 16:59:49.319246
 """
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
-
 # revision identifiers, used by Alembic.
 revision: str = 'efd3fc0b6d0e'
 down_revision: Union[str, Sequence[str], None] = None
